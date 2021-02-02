fdfb946e00c6a2d3dd7d4b9e4dfa42c6c1ffbd06
taka-elle <takaelle0119@gmail.com>
2021-02-02 20:12:07 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,20 @@ class RoomsController < ApplicationController
     # これに入れてねっていう空の変数を定義
     @room = Room.new
   end
+
+  def create
+    # 空の変数
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,6 @@ class Room < ApplicationRecord
   has_many :room_users
   # １部屋は複数のusersをもっている、throughオプションで中間テーブルだと認識
   has_many :users, through: :room_users
+
+  validates :name, presence: true
 end
