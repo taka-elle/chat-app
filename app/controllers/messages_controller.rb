d87f0eb6b9fd3c90b9a1e22c6684d6c144739a0c
taka-elle <takaelle0119@gmail.com>
2021-02-03 01:09:51 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def index
+    @message=Message.new
+    # 指定チャットルームのレコード情報
+    @room=Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    # @room.messagesでルームに紐づいたメッセージ.new(引数)で保存
+    @message = @room.messages.new(message_params)
+    if @message.save
+      # indexへ
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+    
+    
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   # ７つのアクションのルーティングを一気に生成
+  # ユーザー情報と更新のアクション
   resources :users,only:[:edit,:update]
-  resources :rooms,only:[:new,:create]
+  # 部屋の作成と部屋情報を入れるアクション
+  resources :rooms,only:[:new,:create] do
+    # どの部屋のメッセージか知りたいのでroomsの中にネストする
+    # メッセージのやりとりページとメッセージ保存のアクション
+    resources :messages,only:[:index,:create]
+  end
 end
