392cf644cfbf385ea5c007020b491fd12478f6e5
taka-elle <takaelle0119@gmail.com>
2021-02-02 12:44:49 +0900

    ユーザー情報表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   # もし[devise]に関する[controller]の処理であればその時だけ[configure_permitted_parameters]メソッドを実行する
   # 新規登録のnew,createは[devise]の[controller]に記述されている
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   
-
-
   private
   def configure_permitted_parameters #許可されたパラメーターを構成
     #deviseのparamsのようなメソッド    permitで:devise処理名、許可するキーを選択
