1fa6d7c8ae615f8bbbd735d398a40ea56cb428dd
taka-elle <takaelle0119@gmail.com>
2021-02-02 18:09:26 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  # １部屋は複数の中間テーブルをもっている（user情報）
+  has_many :room_users
+  # １部屋は複数のusersをもっている、throughオプションで中間テーブルだと認識
+  has_many :users, through: room_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
+  # １userは複数の中間テーブルを持っている
+  has_many :room_users
+  # １userは複数のroomsテーブルをもっている
+  has_many :rooms, through: room_users
 end
